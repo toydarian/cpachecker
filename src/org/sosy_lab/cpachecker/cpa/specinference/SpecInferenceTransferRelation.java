fce81744a4820485b4595d24d041a036fcc9da99
Thomas Ziegler <zieglert@fim.uni-passau.de>
2015-12-06 11:28:26 +0100

    added unfinished automaton CPA [HAS BUGS]

---
+++ src/org/sosy_lab/cpachecker/cpa/specinference/SpecInferenceTransferRelation.java
@@ -0,0 +1,263 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2015  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *  CPAchecker web page:
+ *    http://cpachecker.sosy-lab.org
+ */
+package org.sosy_lab.cpachecker.cpa.specinference;
+
+import org.sosy_lab.cpachecker.cfa.ast.*;
+import org.sosy_lab.cpachecker.cfa.model.*;
+import org.sosy_lab.cpachecker.core.defaults.ForwardingTransferRelation;
+import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
+import org.sosy_lab.cpachecker.core.interfaces.Precision;
+import org.sosy_lab.cpachecker.cpa.livevar.DeclarationCollectingVisitor;
+import org.sosy_lab.cpachecker.exceptions.CPATransferException;
+
+import java.util.*;
+
+import static com.google.common.collect.FluentIterable.from;
+
+
+public class SpecInferenceTransferRelation
+        extends ForwardingTransferRelation<SpecInferenceState, SpecInferenceState, Precision> {
+
+
+  private static String SEARCH_START = "open";
+  private static String SEARCH_END = "close";
+
+  @Override
+  public Collection<? extends AbstractState> strengthen(AbstractState pState,
+                                                        List<AbstractState> pOtherStates,
+                                                        CFAEdge pCfaEdge,
+                                                        Precision pPrecision) throws CPATransferException, InterruptedException {
+    return null;
+  }
+
+  @Override
+  protected SpecInferenceState handleAssumption(AssumeEdge cfaEdge,
+                                                AExpression expression,
+                                                boolean truthAssumption) throws CPATransferException {
+
+    if (state.getHandle() != null) {
+      CFANode pred = cfaEdge.getPredecessor();
+
+      if (pred.isLoopStart()) {
+
+        return new SpecInferenceState().startTracking(state.getHandle(), "MATCH {$?} -> ASSUME {" + cfaEdge.getCode() + "} ");
+
+      } else if (containsHandle(handleExpression(expression))) {
+        return state.addAutomatonState("MATCH {if($?)} -> ASSUME {" + cfaEdge.getCode() + "} ");
+      }
+    }
+
+    return state;
+  }
+
+
+  @Override
+  protected SpecInferenceState handleFunctionCallEdge(FunctionCallEdge cfaEdge,
+                                                      List<? extends AExpression> arguments,
+                                                      List<? extends AParameterDeclaration> parameters,
+                                                      String calledFunctionName) throws CPATransferException {
+
+    // if we don't have a handle to track, function calls are not of interest
+    if (state.getHandle() != null) {
+
+      Collection<ASimpleDeclaration> vars = handleExpression(arguments);
+
+      // if handle is not contained in parameters, nothing to do
+      if (containsHandle(vars)) {
+        String s = "MATCH {" + cfaEdge.getCode() + "} ->";
+        if (calledFunctionName.contains(SEARCH_END)) {
+          return state.stopTracking(s);
+        } else {
+          return state.addAutomatonState(s);
+        }
+
+      }
+    }
+
+    return state;
+  }
+
+  @Override
+  protected SpecInferenceState handleFunctionReturnEdge(FunctionReturnEdge pCfaEdge,
+                                                        FunctionSummaryEdge pFnkCall,
+                                                        AFunctionCall pSummaryExpr,
+                                                        String pCallerFunctionName) throws CPATransferException {
+    // TODO might be needed
+    return state;
+  }
+
+  @Override
+  protected SpecInferenceState handleDeclarationEdge(ADeclarationEdge pCfaEdge,
+                                                     ADeclaration pDecl) throws CPATransferException {
+    // there won't be anything interesting
+    return state;
+
+  }
+
+  @Override
+  protected SpecInferenceState handleStatementEdge(AStatementEdge pCfaEdge,
+                                                   AStatement statement) throws CPATransferException {
+
+    if (statement instanceof AExpressionAssignmentStatement) {
+      return handleAssignments((AAssignment) statement);
+
+    } else if (statement instanceof AExpressionStatement) {
+      if (state.getHandle() == null) {
+        return state;
+      }
+      AExpressionStatement s = (AExpressionStatement) statement;
+      if (containsHandle(handleExpression(s.getExpression()))) {
+
+        return state.addAutomatonState("MATCH {" + pCfaEdge.getCode() + "} ->");
+
+      }
+
+    } else if (statement instanceof AFunctionCallAssignmentStatement) {
+      return handleAssignments((AAssignment) statement);
+
+    } else if (statement instanceof AFunctionCallStatement) {
+
+      if (state.getHandle() == null) {
+        return state;
+      } else {
+        AFunctionCallStatement s = (AFunctionCallStatement) statement;
+
+        if (containsHandle(handleExpression(s.getFunctionCallExpression().getParameterExpressions()))) {
+          String funcName = ((AIdExpression) s.getFunctionCallExpression().getFunctionNameExpression()).getName();
+
+          if (funcName.contains(SEARCH_END)) {
+            return state.stopTracking("MATCH {" + pCfaEdge.getCode() + "} ->");
+          }
+
+        }
+      }
+
+    } else {
+      throw new CPATransferException("Missing case for if-then-else statement.");
+    }
+
+    return state;
+  }
+
+  @Override
+  protected SpecInferenceState handleReturnStatementEdge(AReturnStatementEdge pCfaEdge) throws CPATransferException {
+
+    // FIXME magic word
+    if (pCfaEdge.getSuccessor().getFunctionName().equals("main")) {
+      return state.addAutomatonState("MATCH EXIT -> ");
+    }
+
+    return state;
+  }
+
+  @Override
+  protected SpecInferenceState handleFunctionSummaryEdge(FunctionSummaryEdge pCfaEdge) throws CPATransferException {
+    // TODO can I ignore this completely?
+    return super.handleFunctionSummaryEdge(pCfaEdge);
+  }
+
+  private Collection<ASimpleDeclaration> handleExpression(List<? extends AExpression> expression) {
+
+    HashSet<ASimpleDeclaration> retval = new HashSet<>();
+
+    for (AExpression e : expression) {
+      retval.addAll(acceptAll(e));
+    }
+
+    return retval;
+  }
+
+  private Collection<ASimpleDeclaration> handleExpression(AExpression expression) {
+    return from(acceptAll(expression)).toSet();
+  }
+
+  private SpecInferenceState handleAssignments(AAssignment as) throws CPATransferException {
+    AIdExpression left = (AIdExpression) as.getLeftHandSide();
+
+    SpecInferenceState succ = null;
+
+    if (as instanceof AExpressionAssignmentStatement) {
+      if (state.getHandle() != null) {
+        if (containsHandle(handleExpression((AExpression) as.getRightHandSide()))) {
+          succ = state.addAutomatonState("MATCH {" + as.toString() + "} -> ");
+        }
+      }
+
+    } else if (as instanceof AFunctionCallAssignmentStatement) {
+      AFunctionCallAssignmentStatement funcStmt = (AFunctionCallAssignmentStatement) as;
+      String funcName = ((AIdExpression) funcStmt.getRightHandSide().getFunctionNameExpression()).getName();
+
+      if (state.getHandle() != null) {
+        if (containsHandle(getParameters(funcStmt.getFunctionCallExpression().getParameterExpressions()))) {
+          String s = "MATCH { $? = " + funcStmt.getRightHandSide().toString() + "} ->";
+          if (funcName.contains(SEARCH_END)) {
+            succ = state.stopTracking(s);
+          } else {
+            succ = state.addAutomatonState(s);
+          }
+        }
+      } else {
+        if (funcName.contains(SEARCH_START)) {
+          succ = state.startTracking(left.getName(), "MATCH {" + as.toString() + "} -> ");
+        }
+      }
+
+    } else {
+      throw new AssertionError("Unhandled assignment type.");
+    }
+
+    if (state.getHandle() != null && left.getName().equals(state.getHandle())) {
+      throw new CPATransferException("Handle is being reassigned!");
+    }
+
+    return succ != null ? succ : state;
+
+  }
+
+  private Collection<ASimpleDeclaration> getParameters(List<? extends AExpression> pParams) {
+    Collection<ASimpleDeclaration> params = new ArrayList<>();
+    for (AExpression expression : pParams) {
+      params.addAll(handleExpression(expression));
+    }
+    return params;
+  }
+
+  private boolean containsHandle(Collection<ASimpleDeclaration> d) {
+    boolean containsHandle = false;
+    for (ASimpleDeclaration e : d) {
+      if (e.getName().equals(state.getHandle())) {
+        containsHandle = true;
+        break;
+      }
+    }
+    return containsHandle;
+  }
+
+  private static Set<ASimpleDeclaration> acceptAll(AExpression exp) {
+    return exp
+                   .<Set<ASimpleDeclaration>, Set<ASimpleDeclaration>, Set<ASimpleDeclaration>, RuntimeException, RuntimeException, DeclarationCollectingVisitor>accept_(
+                           new DeclarationCollectingVisitor());
+  }
+
+}
